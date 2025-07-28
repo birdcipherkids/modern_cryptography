83e1dbb63bd303d2c708b28725aa71c0c5197b1a
Geovanny <birdcode.py@gmail.com>
2025-07-28 16:22:55 -0500

    Encryption and decryption of a message

--- High_level_recipes/Multifernet1.py
+++ High_level_recipes/Multifernet1.py
@@ -1,9 +1,15 @@
 from cryptography.fernet import Fernet, MultiFernet
 
+message = 'Secret message'.encode()
+
 key1 = Fernet(Fernet.generate_key())
 key2 = Fernet(Fernet.generate_key())
 
 f = MultiFernet([key1, key2])
-token = f.encrypt(b'Secret message')
+token = f.encrypt(message)
+
+message_decrypted = f.decrypt(token)
 
 print(token)
+print()
+print(message_decrypted)
