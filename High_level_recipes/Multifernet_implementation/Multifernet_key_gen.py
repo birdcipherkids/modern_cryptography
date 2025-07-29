8e4136c95252fe175e9a30060876dd1c5fd70d54
Geovanny <birdcode.py@gmail.com>
2025-07-28 21:00:31 -0500

    MultiFernet implementation part 1 - key generator settings part 2

--- High_level_recipes/Multifernet_implementation/Multifernet_key_gen.py
+++ High_level_recipes/Multifernet_implementation/Multifernet_key_gen.py
@@ -1,14 +1,18 @@
 from cryptography.fernet import Fernet, MultiFernet
 
-print()
-print()
-print(' ====================================================================================================================== ')
-print()
-print('                                     Welcome to the MultiFernet Key Generator Program                                   ')
-print()
-print(' =======================================================================================================================')
-print()
-print()
+
+def head():
+
+	print()
+	print()
+	print(' ====================================================================================================================== ')
+	print()
+	print('                                     Welcome to the MultiFernet Key Generator Program                                   ')
+	print()
+	print(' =======================================================================================================================')
+	print()
+	print()
+
 
 def key_generator():
 
@@ -34,26 +38,39 @@ def key_generator():
 	return results
 
 
-gen = key_generator()
+
+gen1 = ['Zhw_EkgDs4GMqPsUjVtD-5jdAUeFx4fUcprVwzF-f0M=', 'uzdajqeInFNPFPX39g_l66xwdYp7FXY_I25gqYi3E30=',
+'OpiPA6gSR8WFeWkCLk72Mx2cj0DLJy7fXVZuu7-arkI=']
 
 
-print('    Your encrypted message is:\n    ', gen[0].decode())
-print()
-print()
-print('    The administrative first key (used for encrypting the messages) in the decryption process will be:\n    ', gen[1].decode())
-print()
-print()
-print('    The second key for the decryption process will be:\n    ', gen[2].decode())
-print()
-print()
-print('    The third key for the decryption process will be:\n    ', gen[3].decode())
-print()
-print()
-print(' ······································· proof of decryption of the supplied message ···································')
-print()
-print('    Your decrypted message is:\n    ', gen[4].decode())
-print()
-print(' ·······················································································································')
-print()
-print()
-print('========================================================================================================================')
+def tail():
+
+	print('    Your encrypted message is:\n    ', gen[0].decode())
+	print()
+	print()
+	print('    The administrative first key (used for encrypting the messages) in the decryption process will be:\n    ', gen[1].decode())
+	print()
+	print()
+	print('    The second key for the decryption process will be:\n    ', gen[2].decode())
+	print()
+	print()
+	print('    The third key for the decryption process will be:\n    ', gen[3].decode())
+	print()
+	print()
+	print(' ······································· proof of decryption of the supplied message ···································')
+	print()
+	print('    Your decrypted message is:\n    ', gen[4].decode())
+	print()
+	print(' ·······················································································································')
+	print()
+	print()
+	print('========================================================================================================================')
+
+
+
+
+if __name__=='__main__':
+
+	head()
+	gen = key_generator()
+	tail()
